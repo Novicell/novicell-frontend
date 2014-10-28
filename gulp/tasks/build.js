3a0e7aaead398f440167fd90f5c53f1dc787d9f8
Emil Busch <ebu@novicell.dk>
2014-10-28 14:46:59 +0100

    case 60766: Paths to clean now configurable in config.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,15 +3,16 @@ var config = require('../../gulp-config.json');
 var del = require('del');
 var path = require('path');
 
-gulp.task('clean', function (cb) {
-    var dirs = [config.styles.dist, config.scripts.dist, config.images.dist, config.icons.dist]
-        .map(function (d) { return path.join(config.path, d); });
+gulp.task('clean', function () {
+    var dirs = config.pathsToClean.map(function (d) { 
+        return path.join(config.path, d); 
+    });
 
-    return del(dirs, cb);
+    return del(dirs);
 });
 
 gulp.task('rebuild', ['clean'], function () {
-    return gulp.start(config.buildTasks);
+    return gulp.start('default');
 });
 
-gulp.task('default', config.buildTasks);
+gulp.task('default', config.tasksToBuild);
