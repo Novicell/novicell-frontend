43277a0f008e44a6db6e6b0c0072527845c2327c
Emil Busch <ebu@novicell.dk>
2014-10-31 14:50:37 +0100

    Fixed config paths and, merged livereload with watch task.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 var config = require('../../gulp-config.json');
 var path = require('path');
+var livereload = require('gulp-livereload');
+
+gulp.task("livereload", function () {
+    var paths = config.livereload.paths == null ? [] :
+        config.livereload.paths.map(function (p) {
+            return path.join(config.path, p);
+        });
+
+    livereload.listen(config.livereload.port);
+    return gulp.watch(paths).on('change', livereload.changed);
+});
 
 gulp.task('watch', function () {
     var scriptsPath = config.watch.scripts.map(function (p) {
@@ -23,4 +34,6 @@ gulp.task('watch', function () {
     gulp.watch(stylesPaths, ["styles"]);
     gulp.watch(imagesPaths, ["images"]);
     gulp.watch(iconsPaths, ["icons"]);
+    
+    return gulp.start('livereload');
 });
