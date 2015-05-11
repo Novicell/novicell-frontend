fb9ff25dc16610202d1798da5ea0906459d2d415
Emil Rasmussen <era@novicell.dk>
2015-05-11 15:30:23 +0200

    Added index.js for flattening.

---
+++ index.js
@@ -0,0 +1,10 @@
+(function(root, factory) {
+    if (typeof exports === 'object') {
+        module.exports.VERSION = factory();
+    } else {
+        root.ds = root.ds || {};
+        root.ds.VERSION = factory();
+    }
+}(this, function() {
+    return '1.0.0';
+}));
