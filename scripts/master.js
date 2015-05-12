205bf25100bfdbbf13e9361ce9b022473d87ed3c
Adam Peter Nielsen <apn@MacBook-Pro.local>
2015-05-12 15:16:01 +0200

    initial work on responsive things

--- scripts/master.js
+++ scripts/master.js
@@ -1,5 +1,17 @@
 /**
  * @require ./base/validation.js
+ * @require ./base/responsive.js
  * @require ./components/navigation.js
  * @require ./views/blog.js
  */
+
+$(function () {
+	
+	    novicell.responsive.image();
+
+//    novicell.lazyload.image();
+
+
+}); // document ready end
+
+
