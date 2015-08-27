f96e00161b109644a1e956e68d3de6d186be5cba
Danni Larsen <dla@novicell.dk>
2015-08-27 08:38:42 +0200

    New map function
    In vanilla JS

--- scripts/master.js
+++ scripts/master.js
@@ -14,16 +14,15 @@ var projectName = projectName || {};
 
 // Document ready
 $(function () {
-    // Only include Novicell files that you use!
+    // Only include Novicell functions that you use!
      novicell.responsive.init();
     // novicell.adjustFigureImage.init();
     // novicell.lazyload.content();
     // novicell.lazyload.image();
 
-    // Call new funtions here like this:
-    //projectName.slider.heroSlider($('.owl-carousel'));
-
-        projectName.map.init();
+    // Call new functions here like this:
+    projectName.slider.heroSlider($('.owl-carousel'));
+    projectName.map.init();
 
 }); // Document ready end
 
