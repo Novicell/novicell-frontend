0765b9713f4edbb4e8011d69e23af828fac5efc4
Mark A Lønquist <mlo@novicell.dk>
2016-06-21 13:04:31 +0200

    95359 - Changed watch to auto restart if config.js file is changed. Renamed original watch  to watcher, as the new "watch" is the one triggering the restart.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var config = require('../config.js');
 var plugins = require('gulp-load-plugins')();
+var spawn = require('child_process').spawn;
 
 gulp.task("livereload", function () {
     plugins.livereload.listen(config.livereloadPort);
@@ -9,12 +10,32 @@ gulp.task("livereload", function () {
         .on('change', plugins.livereload.changed);
 });
 
-gulp.task('watch', function () {
+gulp.task('watcher', function () {
     gulp.watch(config.watchScripts, ["scripts:watch"]);
     gulp.watch(config.watchStyles, ["styles"]);
     gulp.watch(config.watchImages, ["images"]);
     gulp.watch(config.watchIcons, ["icons"]);
     gulp.watch(config.watchFonts, ["copy"]);
 
-    return gulp.start('livereload');
+    return;
+});
+
+gulp.task('watch', function () {
+    var watchNode;
+    var livereloadNode;
+
+    gulp.watch(config.projectPath + 'gulp/config.js', spawnChildren);
+    spawnChildren();
+
+    function spawnChildren() {
+        if (watchNode) { watchNode.kill(); }
+        watchNode = spawn('gulp.cmd', ['watcher'], { stdio: 'inherit' });
+
+        if (!livereloadNode) {
+            livereloadNode = spawn('gulp.cmd', ['livereload'], { stdio: 'inherit' });
+        } else {
+            gulp.watch(config.livereloadPaths)
+                .on('change', plugins.livereload.changed);
+        }
+    }
 });
