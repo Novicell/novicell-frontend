ec8981ea123d36c5b8f8558320091ca97807fb8f
Danni Larsen <dla@novicell.dk>
2016-10-11 17:05:25 +0200

    Solve #47 - Convert tabs to spaces
    Solve #47 - Convert tabs to spaces

--- scripts/master.js
+++ scripts/master.js
@@ -6,33 +6,30 @@
 *  ------
 * Dependencies: jquery, novicell.js, other files with awesome functions.
 *  Usage: $(function () {
-*              projectName.slider.heroSlider($('.owl-carousel'));
+*              InwidoAA.slider.heroSlider($('.owl-carousel'));
 *         }); // Document ready end
 */
 
 var novicell = novicell || {};
-var projectName = projectName || {};
+var InwidoAA = InwidoAA || {};
 
 // Document ready
 $(function () {
-	// Only include Novicell functions that you use!
-	novicell.map.init();
-	novicell.cookieInfo.init();
-	novicell.embed.loadEmbeds();
-	//novicell.responsiveLazyloadImage.onLoad();
+    // Only include Novicell functions that you use!
+    novicell.cookieInfo.init();
+    novicell.embed.loadEmbeds();
+    novicell.responsiveLazyloadImage.onLoad();
 
-	// Call new functions here like this:
-	//projectName.slider.heroSlider($('.owl-carousel'));
-	svg4everybody(); // Fix SVG spritemap in IE/Edge
-
+    // Call new functions here like this:
+    //InwidoAA.slider.heroSlider($('.owl-carousel'));
+    svg4everybody(); // Fix SVG spritemap in IE/Edge
 
 
-	novicell.font.webfont({
-		google: { families: ['Roboto:400,700italic:latin', 'Droid+Sans:400,700:latin'] },
-		typekit: { id: ['rzx0prp'] },
-		custom: { families: ['SkipLegDay'], urls: ['/dist/css/webfont.min.css'] }
-	});
-	//novicell.font.local("Danni Er Nice");
+    novicell.font.webfont({
+        google: { families: ['Roboto:300:latin'] },
+        custom: { families: ['AFDiwa-Light'], urls: ['/dist/css/webfont.min.css'] }
+    });
+    //novicell.font.local("Danni Er Nice");
 
 }); // Document ready end
 
@@ -48,22 +45,17 @@ $(function () {
 
 // Window resize (debounced)
 $(window).smartresize(function(e){
-	novicell.responsiveLazyloadImage.onResize();
-	novicell.embed.onResize();
+    novicell.responsiveLazyloadImage.onResize();
+    novicell.embed.onResize();
 }); // Window resize
 
 // Window scroll (debounced)
 $(window).smartscroll(function (e) {
-	// call functions here
-	if ($('.responsiveLazyload').length) {
-		novicell.responsiveLazyloadImage.onScroll();
-	}
-	if ($('.embed-media-item').length) {
-		novicell.embed.loadEmbeds();
-	}
+    // call functions here
+    if ($('.responsiveLazyload').length) {
+        novicell.responsiveLazyloadImage.onScroll();
+    }
+    if ($('.embed-media-item').length) {
+        novicell.embed.loadEmbeds();
+    }
 }); // Window scroll
-
-// // Window mousemove (debounced)
-// $(window).smartmousemove(function(e){
-//     // do stuff as soon as the user stops moving his mouse for longer than 100ms
-// }); // Window mousemove
