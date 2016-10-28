f2c1c3b1580647ee90024eed2644f21f70ca425b
Sergi Mulà <sergi.serch@gmail.com>
2016-10-28 13:50:47 +0200

    Separate statements to avoid confusion
    In order to avoid more confussion like:
    [this commit] (https://github.com/Novicell/novicell-frontend/pull/77/commits/08bf2e2cc6c6cb3afa7cf1963f4959f079631c7c)

--- scripts/components/novicell.lazyloadResponsive.js
+++ scripts/components/novicell.lazyloadResponsive.js
@@ -103,7 +103,8 @@ novicell.responsiveLazyloadImage = novicell.responsiveLazyloadImage || function 
         var $figure = $(figure);
 
         //data variables and images settings
-        var width, maxWidth, heightRatio, focalPoint, mode, filter, quality, height, isBackgroundImage = false, forceHeight = false;
+        var width, maxWidth, heightRatio, focalPoint, mode, filter, quality, height;
+        var isBackgroundImage = false, forceHeight = false;
 
         var $parent = $figure.data('parent') ? $figure.closest($figure.data('parent')) : null;
         isBackgroundImage = $figure.data('is-background') ? true : false;
