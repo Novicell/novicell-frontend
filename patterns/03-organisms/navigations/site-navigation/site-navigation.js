354687bfc786a25785033326a9032bcbdef2a2fd
shp@novicell.dk <shp@novicell.dk>
2018-02-21 13:24:24 +0100

    Site nav desktop and mobile, now with sub navigation

--- patterns/03-organisms/navigations/site-navigation/site-navigation.js
+++ patterns/03-organisms/navigations/site-navigation/site-navigation.js
@@ -6,10 +6,12 @@ novicell.siteNavigation = novicell.siteNavigation || new function () {
     this.init = function () {
         var siteNavigationToggle = document.getElementById('js-site-navigation-toggle');
         var siteNavigationList = document.getElementById('js-site-navigation-list');
-
+        
         if (!siteNavigationToggle || !siteNavigationList) {
             return;
         }
+
+        initSubNavigations(siteNavigationList);
 
         siteNavigationToggle.addEventListener('click', function(e) {
             e.preventDefault();
@@ -19,3 +21,31 @@ novicell.siteNavigation = novicell.siteNavigation || new function () {
         });
     };
 };
+
+function initSubNavigations(siteNavigationList) {
+    var subNavToggles = siteNavigationList.querySelectorAll('.js-sub-navigation-list-toggle');
+    
+    if (!subNavToggles) {
+        return;
+    }
+
+    for(var i = 0; i < subNavToggles.length; i++) {
+        var currentToggle = subNavToggles[i];
+
+        currentToggle.addEventListener('click', function(elem) {
+            elem.preventDefault();
+            var toggle = elem.currentTarget;
+
+            var toggleParent = toggle.parentNode;
+
+            if (!toggleParent) {
+                return;
+            }
+
+            var subList = toggleParent.querySelector('.js-sub-navigation-list');
+
+            novicell.helper.toggleClass(subList, 'sub-navigation-list--open');
+        });
+    }
+
+}
