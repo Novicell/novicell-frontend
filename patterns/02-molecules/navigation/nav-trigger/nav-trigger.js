891b51ee9ad4c5fe7bfb6c9ee2d1884f4ea2904f
NOVICELL\jve <jve@novicell.dk>
2018-07-09 12:18:54 +0200

    Added null check for trigger.

--- patterns/02-molecules/navigation/nav-trigger/nav-trigger.js
+++ patterns/02-molecules/navigation/nav-trigger/nav-trigger.js
@@ -7,19 +7,18 @@ novicell.navTrigger = novicell.navTrigger || new function () {
         var navButton = document.querySelector('#js-mobile-navigation-button');
         var closeButton = document.querySelector('#js-mobile-close-button');
         var body = document.querySelector('#site');
+        if (navButton) {
+            // Open mobile navigation
+            navButton.addEventListener('click', function () {
+                body.classList.toggle('mobile-navigation-open');
+            }, true);
 
-        // Open mobile navigation
-        navButton.addEventListener('click', function () {
-            body.classList.toggle('mobile-navigation-open');
-        }, true);
-
-        // Close
-        closeButton.addEventListener('click', function () {
-            console.log('close');
-            if(body.classList.contains('mobile-navigation-open')){
-                body.classList.remove('mobile-navigation-open');
-            }
-        }, true);
-
+            // Close
+            closeButton.addEventListener('click', function () {
+                if (body.classList.contains('mobile-navigation-open')) {
+                    body.classList.remove('mobile-navigation-open');
+                }
+            }, true);
+        }
     };
 }();
