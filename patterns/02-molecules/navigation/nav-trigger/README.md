07395b0318d2d51d8d30c02f41a02fc4b8aed050
Emil Ankersen (EAN) <emilankersen88@gmail.com>
2018-07-12 12:42:26 +0200

    Styling and animation of mobile menu #223

---
+++ patterns/02-molecules/navigation/nav-trigger/README.md
@@ -0,0 +1,5 @@
+# Hamburger Menu
+
+Require an animation from https://github.com/jonsuh/hamburgers/blob/master/dist/hamburgers.css to work
+
+
--- patterns/02-molecules/navigation/nav-trigger/nav-trigger.js
+++ patterns/02-molecules/navigation/nav-trigger/nav-trigger.js
@@ -5,19 +5,12 @@ var novicell = novicell || {};
 novicell.navTrigger = novicell.navTrigger || new function () {
     this.init = function () {
         var navButton = document.querySelector('#js-mobile-navigation-button');
-        var closeButton = document.querySelector('#js-mobile-close-button');
         var body = document.querySelector('#site');
         if (navButton) {
             // Open mobile navigation
             navButton.addEventListener('click', function () {
                 body.classList.toggle('mobile-navigation-open');
-            }, true);
-
-            // Close
-            closeButton.addEventListener('click', function () {
-                if (body.classList.contains('mobile-navigation-open')) {
-                    body.classList.remove('mobile-navigation-open');
-                }
+                this.classList.toggle('is-active');
             }, true);
         }
     };
