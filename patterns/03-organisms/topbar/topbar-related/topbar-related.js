8795312516d7d36c1c086b7f5bbc65f1e6d2fded
Kasper Ottzen (KOT) <kot@novicell.dk>
2018-08-08 07:53:56 +0200

    Moved function under init

--- patterns/03-organisms/topbar/topbar-related/topbar-related.js
+++ patterns/03-organisms/topbar/topbar-related/topbar-related.js
@@ -4,6 +4,12 @@ var novicell = novicell || {};
 
 novicell.topbarRelated = novicell.topbarRelated || new function () {
 
+    this.init = function () {
+        window.addEventListener('scroll', function () {
+            fixedRelatedTopbar('js-topbar-related', 'sticky', '.js-header', 25);
+        }, false);
+    };
+
     function fixedRelatedTopbar(elementID, className, headerClass, offset) {
         // Get the current scroll position
         let scroll = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
@@ -16,11 +22,5 @@ novicell.topbarRelated = novicell.topbarRelated || new function () {
         if (scroll < headerHeight + offset) {
             el.classList.remove(className);
         }
-    }
-
-    this.init = function () {
-        window.addEventListener('scroll', function () {
-            fixedRelatedTopbar('js-topbar-related', 'sticky', '.js-header', 25);
-        }, false);
     };
 }();
