29f2aefed722eb6428017f3f12e099a1c4ee7dc4
Mark Korfitz Gylling Hansen <mgh@novicell.dk>
2018-11-14 09:32:37 +0100

    Removed @nav-main-list call in related-header-hbs. Fixed issue in topbar-related.js with IF statement

--- patterns/03-organisms/topbar/topbar-related/topbar-related.js
+++ patterns/03-organisms/topbar/topbar-related/topbar-related.js
@@ -13,14 +13,16 @@ novicell.topbarRelated = novicell.topbarRelated || new function () {
     function fixedRelatedTopbar(elementID, className, headerClass, offset) {
         // Get the current scroll position
         let scroll = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        const headerHeight = document.querySelector(headerClass).offsetHeight;
-        const el = document.getElementById(elementID);
-
-        if (scroll >= headerHeight + offset) {
-            el.classList.add(className);
-        }
-        if (scroll < headerHeight + offset) {
-            el.classList.remove(className);
+        const header = document.querySelector(headerClass)
+        if (header) {
+            const headerHeight = header.offsetHeight;
+            const el = document.getElementById(elementID);
+            if (scroll >= headerHeight + offset) {
+                el.classList.add(className);
+            }
+            if (scroll < headerHeight + offset) {
+                el.classList.remove(className);
+            }
         }
     };
 }();
