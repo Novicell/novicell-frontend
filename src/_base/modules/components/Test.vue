757509693d2e817b5ab8d343f4e4a0df17b356f5
Juozas Rastenis <jra@novicell.dk>
2019-01-08 12:48:42 +0100

    vue fix'd

---
+++ src/_base/modules/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>HeyHo, {{text}}</span>
+</template>	
+
+ <script>
+export default {
+  data() {
+    return {
+      count: 1,
+      text: "Let's change the number"
+    };
+  }
+};
+</script>	
+
+ <style>
+</style> 
