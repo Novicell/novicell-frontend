6701cee570cfe5469ab9fe2c7e262b7340ff1dbd
henrikschytze <henrikschytze@gmail.com>
2019-01-08 14:46:13 +0100

    working with handlebars

---
+++ patterns/02-molecules/animate-on-scroll-wrapper/README.md
@@ -0,0 +1,2 @@
+You can put anything inside of this wrapper that you wish to animate. As you render animate-on-scroll-wrapper, you can pass it specific parameters, such as animation, delay and duration.
+The default animation for the wrapper is "zoom-out". A good example of its application can be found under the page: aos-page.
--- patterns/02-molecules/animate-on-scroll-wrapper/animate-on-scroll-wrapper.js
+++ patterns/02-molecules/animate-on-scroll-wrapper/animate-on-scroll-wrapper.js
@@ -4,23 +4,6 @@ novicell.animateOnScroll =
     novicell.animateOnScroll ||
     new function () {
         this.init = () => {
-            let animateWrapper = document.getElementsByClassName(
-                'list-page-item'
-            );
-            if (animateWrapper.length === 0) {
-                return;
-            }
-            for (let i = 0; i < animateWrapper.length; i++) {
-                animateElementOnScroll(animateWrapper[i], {
-                    'data-aos': 'fade-right',
-                    'data-aos-delay': '1500'
-                });
-            }
             AOS.init({});
         };
     }();
-let animateElementOnScroll = (element, args = {}) => {
-    for (let key in args) {
-        element.setAttribute(key, `${args[key]}`)
-    }
-};
