dabadb7da63482ac44408a461ac1bc65a7e10cca
henrikschytze <henrikschytze@gmail.com>
2019-01-11 14:32:02 +0100

    added parallax molecule

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -1,25 +1,26 @@
 'use strict';
 /*
-* Novicell master
-* Use this file to call functions in other files, on document ready, window.resize etc.
-*/
+ * Novicell master
+ * Use this file to call functions in other files, on document ready, window.resize etc.
+ */
 
 var novicell = novicell || {};
 var projectName = projectName || {};
 
 // Document ready
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     novicell.validate.init(); // Init Validation
     novicell.inputMasking.init(); //Init input masking
     novicell.persistentField.init(); //Init persistentField
-    novicell.topbarRelated.init(); 
+    novicell.topbarRelated.init();
     novicell.pageheaderSlider.init();
-    novicell.cookieInfo.init(); 
+    novicell.cookieInfo.init();
     novicell.scrollProgressBar.init();
     novicell.navigation.init();
     novicell.navTrigger.init();
     novicell.pageheaderVideoYoutube.init();
     novicell.pageheaderDots.init();
+    novicell.parallaxWrapper.init();
 
     //SetTimeout to fix latency with loaded elements in e.g. Vue components
     setTimeout(() => {
---
+++ patterns/02-molecules/parallax-wrapper/README.md
@@ -0,0 +1,1 @@
+Place any element inside of this element to apply parallax effect to it. List of available data-attributes can be found at https://github.com/wagerfield/parallax
