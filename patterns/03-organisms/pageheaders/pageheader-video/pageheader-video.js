f38fe9f9df5c1d07e875380b7b5b520d5edbb7d2
henrikschytze <henrikschytze@gmail.com>
2019-02-27 16:38:04 +0100

    altered css classes

--- patterns/03-organisms/pageheaders/pageheader-video/pageheader-video.js
+++ patterns/03-organisms/pageheaders/pageheader-video/pageheader-video.js
@@ -7,10 +7,6 @@ novicell.pageheaderVideo =
     new function () {
         this.init = function () {
             if (screenWidth()) {
-                const wrapper = document.querySelector(".pageheader-video") || false;
-                if (wrapper) {
-                    wrapper.classList.remove("pageheader-video--idle");
-                }
                 const vimeoIframeList = document.querySelectorAll(".vimeo__iframe") || false;
                 const youtubeList = document.querySelectorAll(".video-wrapper[data-youtube-id]") || false;
                 if (vimeoIframeList) {
