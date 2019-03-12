442346418cd54810fa443aa43eaf35f0d1062ee4
Juozas Rastenis <jra@novicell.dk>
2019-03-12 09:39:25 +0100

    Example with Vuejs and sending props to child component

--- src/Modules/components/Test.vue
+++ src/Modules/components/Test.vue
@@ -1,14 +1,24 @@
 <template>
-  <span>HeyHo, {{text}}</span>
-</template>	
-
+  <div>HeyHo, {{text}} and <h5>Multiplied count: {{multiplied}}</h5></div>
+</template>
  <script>
 export default {
+  props: {
+    child_count: {
+      type: Number,
+      required: false
+    }
+  },
   data() {
     return {
       count: 1,
-      text: "Let's change the number"
+      text: "It's the child component!"
     };
+  },
+  computed: {
+    multiplied: function() {
+      return this.child_count * 2;
+    }
   }
 };
 </script>	
